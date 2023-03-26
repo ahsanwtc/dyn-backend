4a677e45aa6f0cd35b69d7c5bed176dc96c0f37f
jsan <ahsan.wtc@gmail.com>
2023-03-26 23:45:19 +0200

    implementing login

--- functions/login.js
+++ functions/login.js
@@ -1,13 +1,35 @@
+const AWS = require('aws-sdk');
+const cognito = new AWS.CognitoIdentityServiceProvider();
+
+const { validateInput, sendResponse } = require('../util');
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'LOGIN',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    const data = JSON.parse(event.body);
+
+    const isValid = validateInput(data);
+    if (!isValid) {
+      return sendResponse(400, { message: 'Invalid input' });
+    }
+
+    const { email, password } = data;
+    const { USER_POOL_ID, USER_POOL_CLIENT_ID } = process.env;
+    
+    const params = {
+      AuthFlow: "ADMIN_NO_SRP_AUTH",
+      UserPoolId: USER_POOL_ID,
+      ClientId: USER_POOL_CLIENT_ID,
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: password
+      }
+    };
+
+    const response = await cognito.adminInitiateAuth(params).promise();
+    return sendResponse(200, { message: 'Success', token: response.AuthenticationResult.IdToken })
+
+  } catch (error) {
+    const message = error.message ? error.message : 'Internal server error';
+    return sendResponse(500, { message });
+  }
 };
